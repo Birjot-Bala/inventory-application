a30dd3bd87090170ed968f3153baaf47a316e05f
Birjot Bala <birjot.bala@gmail.com>
2021-03-13 16:42:25 -0500

    Define models for categories and items

---
+++ models/category.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const CategorySchema = new Schema({
+    name: {type: String, required: true},
+    desc: {type: String, required: true},
+});
+
+
+// Virtual for category URL.
+CategorySchema
+    .virtual('url')
+    .get(function() {
+        return `/inventory/category/${this._id}`;
+    });
---
+++ models/item.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true},
+    desc: {type: String, required: true},
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+    price: {type: Decimal128, required: true},
+    stock: {type: Number, required: true},
+});
+
+// Virtual for item URL.
+ItemSchema
+    .virtual('url')
+    .get(function() {
+        return `/inventory/item/${this._id}`;
+    });
+
+export default mongoose.models('ItemSchema', ItemSchema);
