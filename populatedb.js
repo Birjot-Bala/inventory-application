64ec7dd6d6217aa5fd3cf311e159a96bc3df3d74
Birjot Bala <birjot.bala@gmail.com>
2021-03-13 20:36:18 -0500

    Script to populate db with test items

--- populatedb.js
+++ populatedb.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
+console.log('This script populates some test items and categories to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
 
 // Get arguments passed on command line
 const userArgs = process.argv.slice(2);
@@ -9,16 +9,38 @@ import Item from './models/item.js';
 import Category from './models/category.js';
 
 // Connect to DB
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 const mongoDB = userArgs[0];
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .catch(err => console.log(err));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+    console.log('Connected to DB')
+    createCategories()
+        .then(() => createItems())
+        .then(() => mongoose.connection.close())
+        .catch(err => console.log('FINAL ERR: ' + err));
+});
 
 const items = [];
 const categories = [];
 
-const createItem = (itemName, desc, category, price, stock) => {
+function createCategory(categoryName, desc) {
+    const category = new Category({
+        name: categoryName,
+        desc: desc
+    });
+
+    return category
+        .save()
+        .then(() => {
+            console.log('New Category: ' + category);
+            categories.push(category);
+        }).catch(err => console.log('ERROR CREATING category: ' + category));
+}
+
+function createItem(itemName, desc, category, price, stock) {
     const item = new Item({
         name: itemName,
         desc: desc,
@@ -27,47 +49,27 @@ const createItem = (itemName, desc, category, price, stock) => {
         stock: stock
     });
 
-    // Return promise
-    return (
-        item
-            .save()
-            .then(() => {
-                console.log('New Item: ' + item);
-                items.push(item);
-            })
-    );
+    return item
+        .save()
+        .then(() => {
+            console.log('New Item: ' + item);
+            items.push(item);
+        }).catch(err => console.log('ERROR CREATING item: ' + item));
 }
 
-const createCategory = (categoryName, desc) => {
-    const category = new Category({
-        name: categoryName,
-        desc: desc
-    });
-
-    // Return promise
-    return (
-        category
-            .save()
-            .then(() => {
-                console.log('New Category: ' + category);
-                categories.push(category);
-            })
-    );
-}
-
-const createCategories = async () => {
-    Promise.all([
+function createCategories() {
+    return Promise.all([
         createCategory('Baked Goods', 'Freshly baked goods that will leave your sweet tooth satisfied.'),
         createCategory('Burgers', 'Variety of burgers for every type of person!'),
         createCategory('Sides', 'Accompany your main with classic sides!'),
     ]).then(() => console.log("Succesfully created categories"))
-    .catch(err => {
-        console.error(err);
-    })
+        .catch(err => {
+            console.error(err);
+        });
 }
 
-const createItems = async () => {
-    Promise.all([
+function createItems() {
+    return Promise.all([
         createItem('Donut', 'Classic glazed donut.', categories[0], '0.75', 12),
         createItem('Carrot Muffin', 'Muffin made from fresh carrots', categories[0], '0.75', 15),
         createItem('Cupcake', 'Vanilla frosting cupcake!', categories[0], '0.50', 30),
@@ -76,14 +78,9 @@ const createItems = async () => {
         createItem('Fries', 'Classic cut fries.', categories[2], '1.00', 50),
         createItem('Mashed Potatoes', 'Famous mashed potatoes with gravy', categories[2], '2.00', 40)
     ]).then(() => console.log('Successfully created items'))
-    .catch(err => {
-        console.error(err);
-    })
+        .catch(err => {
+            console.error(err);
+        });
 }
 
 // Carry out creation in series as items depend on categories.
-await createCategories();
-await createItems();
-
-// Completed. Disconnect from database.
-mongoose.connection.close();
